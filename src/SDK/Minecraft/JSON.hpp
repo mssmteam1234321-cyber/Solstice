7e9e590ce57c8325907774777b6e0c0e2561b5bb
Tozic Gaming <isameldein2012@gmail.com>
2024-08-13 16:32:40 +0400

    Integrated Json::Value member variables and relevant constructs

--- src/SDK/Minecraft/JSON.hpp
+++ src/SDK/Minecraft/JSON.hpp
@@ -11,8 +11,65 @@
 
 namespace MinecraftJson
 {
+    enum class ValueType : int32_t {
+        Null    = 0x0,
+        Int     = 0x1,
+        Uint    = 0x2,
+        Real    = 0x3,
+        String  = 0x4,
+        Boolean = 0x5,
+        Array   = 0x6,
+        Object  = 0x7,
+    };
+
     class Value { // equivalent to bds' Json::Value, but we dont wanna confuse it with nlohmann json
+    //public:
         char pad[16];
+        /*struct CZString {
+            enum class DuplicationPolicy : unsigned int { // mPolicy?? got this from BDS
+                noDuplication   = 0,
+                duplicate       = 1,
+                duplicateOnCopy = 2,
+                Mask            = 3,
+            };
+
+            struct StringStorage {
+                DuplicationPolicy mPolicy : 2;     // this+0x0
+                unsigned int      mLength : 30;    // this+0x0 // 1GB Max cuz this sux
+            };
+
+            const char *mStr;       // this+0x4
+            uint32_t mIndex;        // this+0x8 // ArrayIndex    mIndex{};
+            StringStorage mStorage; // this+0xC
+
+
+            //union {
+            //    ArrayIndex    mIndex{};
+            //    StringStorage mStorage;
+            //};
+
+
+
+
+
+            CZString() : mStr(nullptr), mIndex(0) {}
+            CZString(const char *s, uint32_t index) : mStr(s), mIndex(index) {}
+        };
+
+        union ValueHolder {
+            int64_t                   mInt;       // this+0x0
+            uint64_t                  mUInt64;    // this+0x8 // idk
+            long double               mReal;      // this+0x10
+            bool                      mBool;      // this+0x18
+            char                      *mString;   // this+0x1C
+            std::map<CZString, Value> *mMap;      // this+0x20
+        };
+
+        ValueHolder mValue;                       // this+0x0000
+
+        ValueType   mType : 8;                    // this+0x0028
+        int32_t     mAllocated : 1;               // this+0x002C
+        */
     public:
         std::string* toStyledString()
         {
