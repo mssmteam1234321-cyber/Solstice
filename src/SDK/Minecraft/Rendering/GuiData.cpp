09fc9435d8874da3d10e971bd8e201ecffd02053
vastrakai <95504366+VastraKai@users.noreply.github.com>
2024-08-18 02:49:00 -0400

    sendToSelf abuse for displayClientMessage

--- src/SDK/Minecraft/Rendering/GuiData.cpp
+++ src/SDK/Minecraft/Rendering/GuiData.cpp
@@ -4,8 +4,11 @@
 
 #include "GuiData.hpp"
 
+#include <Hook/Hooks/NetworkHooks/PacketReceiveHook.hpp>
 #include <SDK/SigManager.hpp>
 #include <SDK/Minecraft/ClientInstance.hpp>
+#include <SDK/Minecraft/GameSession.hpp>
+#include <SDK/Minecraft/MinecraftSim.hpp>
 #include <SDK/Minecraft/Network/MinecraftPackets.hpp>
 #include <SDK/Minecraft/Network/Packets/TextPacket.hpp>
 #include <Utils/MemUtils.hpp>
@@ -13,6 +16,19 @@
 void GuiData::displayClientMessage(const std::string& msg)
 {
     if (!ClientInstance::get()->getLocalPlayer()) return;
-    static int lol = 0;
-    MemUtils::callFastcall<void>(SigManager::GuiData_displayClientMessage, this, msg, &lol, false);
-}
+    static uintptr_t func = 0;
+
+    auto textPacket = MinecraftPackets::createPacket<TextPacket>();
+    textPacket->mMessage = msg;
+    textPacket->mType = TextPacketType::Raw;
+
+    static bool once = false;
+    if (!once || !func)
+    {
+        func = textPacket->mDispatcher->getPacketHandler();
+        once = true;
+    }
+
+    if (!func) return;
+    MemUtils::callFastcall<void>(func, textPacket->mDispatcher, PacketReceiveHook::NetworkIdentifier, ClientInstance::get()->getMinecraftSim()->getGameSession()->getEventCallback(), textPacket);
+};
