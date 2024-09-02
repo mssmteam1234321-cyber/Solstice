be3460e71620e4070ad65b73702c7cdd5bf9d95f
vastrakai <95504366+VastraKai@users.noreply.github.com>
2024-09-02 15:37:11 -0400

    Test scaffold bypass, undo if this doesn't work

--- src/Features/Modules/Player/Scaffold.cpp
+++ src/Features/Modules/Player/Scaffold.cpp
@@ -331,7 +331,29 @@ void Scaffold::onPacketOutEvent(PacketOutEvent& event)
         {
             const auto transac = reinterpret_cast<ItemUseInventoryTransaction*>(it->mTransaction.get());
             if (transac->mActionType == ItemUseInventoryTransaction::ActionType::Place)
-                transac->mClickPos = glm::vec3(0, 0, 0);
+            {
+                spdlog::info("face: {}, clickPos: ({}, {}, {})", transac->mFace, transac->mClickPos.x, transac->mClickPos.y, transac->mClickPos.z);
+                //transac->mClickPos = (transac->mPlayerPos - glm::vec3(0, 1.62, 0)) - glm::vec3(transac->mBlockPos);
+                if (transac->mFace == 0) // Down
+                {
+                    transac->mClickPos = glm::vec3(0.5, -0, 0.5);
+                } else if (transac->mFace == 1) // Up
+                {
+                    transac->mClickPos = glm::vec3(0.5, 1, 0.5);
+                } else if (transac->mFace == 2) // North
+                {
+                    transac->mClickPos = glm::vec3(0.5, 0.5, 0);
+                } else if (transac->mFace == 3) // South
+                {
+                    transac->mClickPos = glm::vec3(0.5, 0.5, 1);
+                } else if (transac->mFace == 4) // West
+                {
+                    transac->mClickPos = glm::vec3(0, 0.5, 0.5);
+                } else if (transac->mFace == 5) // East
+                {
+                    transac->mClickPos = glm::vec3(1, 0.5, 0.5);
+                }
+            }
         }
     }
 
