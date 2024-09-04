8e897e05c900143e5dfc220e6fbc03c77cc10163
vastrakai <95504366+VastraKai@users.noreply.github.com>
2024-09-03 20:44:18 -0400

    shit

--- src/Features/Modules/Misc/IRC.cpp
+++ src/Features/Modules/Misc/IRC.cpp
@@ -6,6 +6,7 @@
 
 #include <Features/IRC/IrcClient.hpp>
 #include <SDK/Minecraft/ClientInstance.hpp>
+#include <Utils/OAuthUtils.hpp>
 
 void IRC::onEnable()
 {
@@ -13,6 +14,14 @@ void IRC::onEnable()
     if (!player)
     {
         NotifyUtils::notify("Please don't enable IRC until you're in game!", 10.0f, Notification::Type::Error);
+        setEnabled(false);
+        return;
+    }
+
+    if (!OAuthUtils::hasValidToken())
+    {
+        ChatUtils::displayClientMessageRaw("§7[§dirc§7] §eFailed to authenticate with Discord. Please authenticate with the injector.");
+        NotifyUtils::notify("Failed to authenticate with Discord. Please authenticate with the injector.", 10.0f, Notification::Type::Error);
         setEnabled(false);
         return;
     }
