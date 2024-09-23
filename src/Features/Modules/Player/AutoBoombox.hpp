95cfa149a8e02263ba339f9cb4522b4f77300338
vastrakai <95504366+VastraKai@users.noreply.github.com>
2024-09-23 12:00:30 -0400

    🐒🐒🐒🐒🐒🐒

--- src/Features/Modules/Player/AutoBoombox.hpp
+++ src/Features/Modules/Player/AutoBoombox.hpp
@@ -12,7 +12,7 @@ public:
         TNT
     };
 
-    EnumSettingT<Mode> mMode = EnumSettingT<Mode>("Mode", "The mode for uncover", Mode::Boombox, "Boombox", "TNT");
+    EnumSettingT<Mode> mMode = EnumSettingT<Mode>("Mode", "The mode to use", Mode::Boombox, "Boombox", "TNT");
     NumberSetting mDelay = NumberSetting("Delay", "The delay in milliseconds to place boombox", 1000, 100, 1500, 10);
     BoolSetting mHotbarOnly = BoolSetting{ "Hotbar Only", "Only switch to boomboxes in the hotbar", true };
     AutoBoombox() : ModuleBase("AutoBoombox", "Automatically places boombox in the hive", ModuleCategory::Player, 0, false)
@@ -22,10 +22,10 @@ public:
         addSetting(&mHotbarOnly);
 
         mNames = {
-              {Lowercase, "autoboombox"},
-                {LowercaseSpaced, "auto boombox"},
-                {Normal, "AutoBoombox"},
-                {NormalSpaced, "Auto Boombox"}
+            {Lowercase, "autoboombox"},
+              {LowercaseSpaced, "auto boombox"},
+              {Normal, "AutoBoombox"},
+              {NormalSpaced, "Auto Boombox"}
         };
     };
 
