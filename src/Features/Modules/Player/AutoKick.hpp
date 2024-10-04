4d5944aeda6e43edf5dce4bba96c5d9f8a1c72ec
DarkNBTHax <DarkNBTHaxLegitAC@outlook.jp>
2024-10-04 23:47:43 +0900

    funny module

--- src/Features/Modules/Player/AutoKick.hpp
+++ src/Features/Modules/Player/AutoKick.hpp
@@ -9,17 +9,20 @@ class AutoKick : public ModuleBase<AutoKick> {
 public:
     enum class Mode {
         Push,
+        Replace
     };
 
-    EnumSettingT<Mode> mMode = EnumSettingT<Mode>("Mode", "The mode for auto kick", Mode::Push, "Push");
+    EnumSettingT<Mode> mMode = EnumSettingT<Mode>("Mode", "The mode for auto kick", Mode::Push, "Push", "Replace");
     NumberSetting mRange = NumberSetting("Range", "The range at which to push entities", 5, 3, 8, 0.1);
     NumberSetting mDelay = NumberSetting("Delay", "The delay in ms to place block", 1000, 100, 5000, 10);
-    BoolSetting mHotbarOnly = BoolSetting("Hotbar Only", "Only switch to boomboxes in the hotbar", true);
+    BoolSetting mHotbarOnly = BoolSetting("Hotbar Only", "Only switch to blocks in the hotbar", true);
     BoolSetting mAllowDiagonal = BoolSetting("Allow Diagonal", "Allow diagonal prediction", true);
     BoolSetting mOnGroundOnly = BoolSetting("OnGround Only", "Place block while target is onground", true);
     NumberSetting mMaxOnGroundTicks = NumberSetting("OnGround Ticks", "The max onground ticks to place block", 10, 0, 100, 5);
     NumberSetting mOpponentPing = NumberSetting("Opponent Ping", "Specify opponent ping time", 90, 0, 300, 10);
     BoolSetting mSpamPlace = BoolSetting("Spam Place", "Reset delay if block ghosted", false);
+    NumberSetting mDestroySpeed = NumberSetting("Destroy Speed", "The destroy speed for fast mine", 1, 0.01, 1, 0.01);
+    BoolSetting mInfiniteDurability = BoolSetting("Infinite Durability", "Infinite durability for tools (may cause issues!)", false);
     BoolSetting mDebug = BoolSetting("Debug", "Send debug message in chat", false);
     AutoKick() : ModuleBase("AutoKick", "Attempts to kick players by placing blocks in front of them", ModuleCategory::Player, 0, false)
     {
@@ -32,6 +35,8 @@ public:
         addSetting(&mMaxOnGroundTicks);
         addSetting(&mOpponentPing);
         addSetting(&mSpamPlace);
+        addSetting(&mDestroySpeed);
+        addSetting(&mInfiniteDurability);
         addSetting(&mDebug);
 
         VISIBILITY_CONDITION(mMaxOnGroundTicks, mOnGroundOnly.mValue);
@@ -45,6 +50,8 @@ public:
     };
 
     int mPreviousSlot = 0;
+
+    // Push
     bool mSelectedSlot = false;
     bool mShouldRotate = false;
 
@@ -65,8 +72,23 @@ public:
 
     std::map<std::string, int> mFlagCounter;
 
+    // Replace
+    std::vector<glm::ivec3> mMiningBlocks;
+    glm::ivec3 mCurrentBlockPos = { INT_MAX, INT_MAX, INT_MAX };
+    int mCurrentBlockFace = 0;
+    int mToolSlot = 0;
+    bool mIsMiningBlock = false;
+    bool mShouldRotateToBlock = false;
+    bool mShouldSpoofSlot = false;
+    bool mShouldSetbackSlot = false;
+    bool mWasPlacingBlock = false;
+    float mBreakingProgress = 0.f;
+
     void onEnable();
     void onDisable() override;
+    void reset();
+    bool isValidBlock(glm::ivec3 blockPos);
+    void queueBlock(glm::ivec3 blockPos);
     void onBaseTickEvent(class BaseTickEvent& event);
     void onPacketOutEvent(class PacketOutEvent& event);
     void onPacketInEvent(class PacketInEvent& event);
