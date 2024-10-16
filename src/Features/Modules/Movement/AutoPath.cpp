773c5fc1f1848cbe8dd0e2704f4cf033077e8890
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2024-10-16 13:11:21 -0400

    stuff start

--- src/Features/Modules/Movement/AutoPath.cpp
+++ src/Features/Modules/Movement/AutoPath.cpp
@@ -4,9 +4,71 @@
 
 #include "AutoPath.hpp"
 
+#include <SDK/Minecraft/ClientInstance.hpp>
+
+BlockSource* AutoPath::cachedSrc = nullptr;
+const float AutoPath::SQRT_2 = sqrtf(2.f);
+
+std::vector<BlockPos> AutoPath::sideAdj = {
+    {1, 0, 0},
+    {0, 0, 1},
+    {-1, 0, 0},
+    {0, 0, -1}
+};
+
+std::vector<BlockPos> AutoPath::directAdj = {
+    {1, 0, 0},
+    {0, 0, 1},
+    {-1, 0, 0},
+    {0, 0, -1},
+    {0, 1, 0},
+    {0, -1, 0}
+};
+
+std::vector<BlockPos> AutoPath::diagAdd = {
+    {1, 0, 1},
+    {-1, 0, 1},
+    {-1, 0, -1},
+    {1, 0, -1}
+};
+
+struct ScoreList {
+    float gScore;
+    float fScore;
+
+    ScoreList() {
+        this->gScore = 694206942069420.f;
+        this->fScore = 694206942069420.f;
+    }
+
+    ScoreList(const float g, const float f) {
+        this->gScore = g;
+        this->fScore = f;
+    }
+};
+
+static glm::vec3* getPlayerHitboxPathPosOffsets() {
+    static glm::vec3 res[8] = {
+        {0.17f, -0.99f, 0.17f},
+        {0.83f, -0.99f, 0.17f},
+        {0.83f, -0.99f, 0.83f},
+        {0.17f, -0.99f, 0.83f},
+        {0.17f, 0.9f, 0.17f},
+        {0.83f, 0.9f, 0.17f},
+        {0.83f, 0.9f, 0.83f},
+        {0.17f, 0.9f, 0.83f}
+    };
+
+    return res;
+}
+
 void AutoPath::onEnable()
 {
+    if (ClientInstance::get()->getLocalPlayer() == nullptr)
+        setEnabled(false);
 
+    posList.clear();
+    ticks = 0;
 }
 
 void AutoPath::onDisable()
--- src/Features/Modules/Movement/AutoPath.hpp
+++ src/Features/Modules/Movement/AutoPath.hpp
@@ -6,10 +6,19 @@
 
 class AutoPath : public ModuleBase<AutoPath>
 {
+    std::vector<glm::vec3> posList{};
+    int ticks = 0;
+
 public:
     AutoPath() : ModuleBase("AutoPath", "Automatically pathfinds to a specified location", ModuleCategory::Movement, 0, false) {
 
     }
+
+    static BlockSource* cachedSrc;
+    static const float SQRT_2;
+    static std::vector<BlockPos> sideAdj;
+    static std::vector<BlockPos> directAdj;
+    static std::vector<BlockPos> diagAdd;
 
     void onEnable() override;
     void onDisable() override;
