4c7f5d8dc55689a9d6e95b40fc7cca2834bc88fc
alteik <alteik1337@gmail.com>
2024-10-22 19:04:08 +0100

    whyy is it soo fucking fat

--- src/SDK/SigManager.hpp
+++ src/SDK/SigManager.hpp
@@ -104,7 +104,7 @@ public:
     DEFINE_SIG(GetSpeedInAirWithSprint, "41 C7 40 ? ? ? ? ? F6 02", SigType::Sig, 0);
     DEFINE_SIG(ConnectionRequest_create_DeviceModel, "48 8B 11 48 83 C2 ? EB", SigType::Sig, 0);
     DEFINE_SIG(ConnectionRequest_create_DeviceOS, "74 ? 41 80 3F ? 74 ? 49 8B ? 49 8B ? 48 8B ? ? ? ? ? FF 15", SigType::Sig, 0);
-    DEFINE_SIG(ConnectionRequest_create_DefaultInputMode, "74 1B 49 83 3E 00", SigType::Sig, 0);
+    DEFINE_SIG(ConnectionRequest_create_DefaultInputMode, "74 ? 80 3E ? 74 ? 49 8B ? 49 8B ? 48 8B ? ? ? ? ? FF 15 ? ? ? ? EB ? 33 C0 C6 44 24 58 ? 48 63 ? 48 89 ? ? ? 48 8D ? ? ? ? ? 48 8D ? ? ? ? ? E8 ? ? ? ? 48 8B ? 33 D2 48 8D ? ? ? E8 ? ? ? ? 0F B6 ? ? 0F B6", SigType::Sig, 0);
     DEFINE_SIG(GetInputModeBypass, "8B D7 48 8B CE 48 8B 80 ? ? ? ? FF 15 ? ? ? ? 49 8B 06", SigType::Sig, 0);
     DEFINE_SIG(MobileRotations, "48 8b 5c 24 ? 89 07 48 83 c4 ? 5f c3 f2 0f 10 02", SigType::Sig, 0);
     DEFINE_SIG(TapSwingAnim, "F3 44 ? ? ? ? ? ? ? 41 0F ? ? F3 0F ? ? ? ? C6 40 38 ? 48 8B ? 41 0F ? ? E8 ? ? ? ? 45 84 ? 0F 85 ? ? ? ? 48 8B", SigType::RefSig, 5);
