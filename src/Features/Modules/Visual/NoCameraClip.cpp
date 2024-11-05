1b709b78cc15bab59823b472d1a413d46d31d7ec
vastrakai <95504366+VastraKai@users.noreply.github.com>
2024-11-05 12:38:43 -0500

    Fix lookinputevent

--- src/Features/Modules/Visual/NoCameraClip.cpp
+++ src/Features/Modules/Visual/NoCameraClip.cpp
@@ -32,5 +32,4 @@ void NoCameraClip::onLookInputEvent(LookInputEvent& event)
     event.mFirstPersonCamera->mFov.z = 2499.0f;
     event.mThirdPersonCamera->mFov.z = 2499.0f;
     event.mThirdPersonFrontCamera->mFov.z = 2499.0f;
-    spdlog::info("NoCameraClip enabled");
 }
