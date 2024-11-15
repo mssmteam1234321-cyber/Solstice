7e3a4228911a6b3cefe178241f1a30e285563429
vastrakai <95504366+VastraKai@users.noreply.github.com>
2024-11-15 13:33:44 -0500

    temporary playSoundFromEmbeddedResource fix

--- src/Utils/MiscUtils/SoundUtils.cpp
+++ src/Utils/MiscUtils/SoundUtils.cpp
@@ -15,22 +15,30 @@ Audio audioManager;
 
 void SoundUtils::playSoundFromEmbeddedResource(std::string resourceName, float volume)
 {
-    if (!ResourceLoader::Resources.contains(resourceName))
-    {
-        spdlog::error("Resource {} not found", resourceName);
-        return;
-    }
-    Resource& soundResource = ResourceLoader::Resources[resourceName];
-    std::string path = FileUtils::getSolsticeDir() + "Audio\\" + resourceName;
-    audioManager.BasePath = FileUtils::getSolsticeDir() + "Audio\\";
-    if (!FileUtils::fileExists(path))
-    {
-        std::ofstream file(path, std::ios::binary);
-        file.write(reinterpret_cast<const char*>(soundResource.data()), soundResource.size());
-        file.close();
-        spdlog::info("Dumped reosurce {} to {}", resourceName, path);
-    }
+    static bool soundsDisabled = false;
+    if (soundsDisabled) return;
 
-    audioManager.Play(resourceName, volume, false);
+    if(!TRY_CALL([&]() {
+        if (!ResourceLoader::Resources.contains(resourceName))
+        {
+            spdlog::error("Resource {} not found", resourceName);
+            return;
+        }
+        Resource& soundResource = ResourceLoader::Resources[resourceName];
+        std::string path = FileUtils::getSolsticeDir() + "Audio\\" + resourceName;
+        audioManager.BasePath = FileUtils::getSolsticeDir() + "Audio\\";
+        if (!FileUtils::fileExists(path))
+        {
+            std::ofstream file(path, std::ios::binary);
+            file.write(reinterpret_cast<const char*>(soundResource.data()), soundResource.size());
+            file.close();
+            spdlog::info("Dumped reosurce {} to {}", resourceName, path);
+        }
+
+        audioManager.Play(resourceName, volume, false);
+    }))
+    {
+        NotifyUtils::notify("An error has occurred while trying to play " + resourceName + "\nSounds will be disabled for the rest of the session", 20.f, Notification::Type::Error);
+        soundsDisabled = true;
+    }
 }
-
